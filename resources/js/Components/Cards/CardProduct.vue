7d7d52807eb362e5a6f1cfc4a41ada17bfc97933
ramosramosramos <ramosramosramosramos12@gmail.com>
2024-08-29 11:57:31 +0800

    done card product

--- resources/js/Components/Cards/CardProduct.vue
+++ resources/js/Components/Cards/CardProduct.vue
@@ -5,13 +5,13 @@
     class="grid justify-center w-[250px] shadow-md text-[var(--mainColor)]"
   >
   <img :src="product.image" alt="wala">
-    <h1>{{ product.name }}</h1>
-    <h1>{{ product.type }}</h1>
-    <h1>{{ product.price }}</h1>
-    <details>
-      <summary>See more details</summary>
+    <p> <strong>Name: </strong> {{ product.name }}</p>
+    <p> <strong>Type: </strong>{{ product.type }}</p>
+    <p><strong>Price: </strong> {{ product.price }}</p><br>
+    <details >
+      <summary class="list-none cursor-pointer hover:text-gray-900">See more details</summary>
 
-      <h1>{{ product.description }}</h1>
+      <p>{{ product.description }}</p>
       <Link :href="route('products.destroy',product.id)" method="delete" as="button" type="button"
         class="bg-red-600 hover:bg-red-700 transition-all outline-none ease-in-out duration-500 text-gray-200 px-2 rounded-sm m-1"
       >
